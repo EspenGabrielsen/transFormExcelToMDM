6f06f6ad41ce88eae56e68b42a7186f964744496
Espen Gabrielsen <espen.gabrielsen@optimera.no>
2025-03-13 14:03:47 +0100

    Thor, ny version

--- KonverterFilerTilMDMFormat.py
+++ KonverterFilerTilMDMFormat.py
@@ -1,13 +1,14 @@
 import openpyxl
-import openpyxl.workbook
+import warnings
 from tkinter import filedialog, messagebox
 import json
 import os
-from pathlib import Path
+import datetime 
 
+# Deaktiver advarsler fra openpyxl som alltid dukker opp pga. macro i excel-innlesning?
+warnings.filterwarnings("ignore", category=UserWarning, module='openpyxl')
 
 class transformExcel:
-
     def getInputData(self):
         self.RowsConfig = ""
 
@@ -18,14 +19,13 @@ class transformExcel:
                 except ValueError as e:
                     print(e, "\nConfig filen virker å være korrupt")
         except FileNotFoundError as e:
-            print(e, "\nkunne ikke finne config filen")
+            print(e, "\nKunne ikke finne config filen")
 
         self.filename = filedialog.askopenfilename(
-            filetypes=[("Excel files", "*.xlsx")], title="Select file"
+            filetypes=[("Excel-filer", "*.xlsx")], title="Velg fil"
         )
 
     def variableSetup(self):
-
         if self.RowsConfig and self.filename:
             self.StartRow = self.RowsConfig.get("startrow") or 1
             self.outPutRow = self.RowsConfig.get("outPutRow") or 1
@@ -42,83 +42,91 @@ class transformExcel:
                 self.ws = None
 
     def setupNewFile(self):
-        self.new_wb = openpyxl.load_workbook(r"C:\Users\E1732506\Downloads\3e37eb57-6054-43b1-baa9-8b510a4f61e9_out.xlsm") # legg inn path til mal Thor Isum
-        newsheet = self.RowsConfig.get("OutSheet") or self.new_wb.active.title
-        #if not newsheet in self.new_wb.worksheets:
-        #    print(newsheet)     
-        #    self.new_wb.create_sheet(newsheet, 0)
-        self.new_ws = self.new_wb[newsheet]
+        # Last inn malen for hvor data skal legges inn
+        template_file = filedialog.askopenfilename(
+            title="Velg malfil der varene skal leses inn",
+            filetypes=[("Excel-filer", "*.xlsm;*.xlsx")]
+        )
+
+        self.new_wb = openpyxl.load_workbook(template_file, keep_vba=True)  # Behold makroer
+
+        # Sørg for at kun "Entities"-arket er til stede i innlesningsmalen.
+        if "Entities" in self.new_wb.sheetnames:
+            self.new_ws = self.new_wb["Entities"]
+        else:
+            messagebox.showwarning("Ark mangler", message="Entities-arket ble ikke funnet i malen.")
+            return
+
+        # Fjern alle andre ark enn "Help" og "Entities"
+        for sheet in self.new_wb.sheetnames:
+            if sheet not in ["Help", "Entities"]:
+                del self.new_wb[sheet]  # Fjern uønskede ark
 
     def readAndWriteExcel(self):
+        # Start med å sette gjeldende rad til output-rad
+        self.NewWorkBookCurrentRow = self.outPutRow
+        self.deleteIfRowFails = []
         for i, self.rowSelected in enumerate(self.RowsConfig["Rows"]):
             for row in range(10_000):
-
-                self.deleteIfRowFailes = [] # stores column and row so we can delete if flagg skipRowIfMissing is triggered
+                
+                # Beregn den nye raden i output-arbeidsboken basert på offset
                 offset = (self.itemOfset * row) + self.skippedRows
+
                 self.NewWorkBookCurrentRow = self.outPutRow + self.TargetRowOffset[i] + offset
 
+                # Stopp hvis PrimaryKeyColumn er tom
                 if not self.ws[self.PrimaryKeyColumn + str(self.StartRow + row)].value:
-                    break  # Stoppes if PrimaryKeyColumn is empty
+                    break  # Stopp hvis PrimaryKeyColumn er tom
+
+                # Skriv data til den nye arbeidsboken
                 for col in self.rowSelected.keys():
-                    self.skipRowIfMissingFlagg = self.rowSelected[col].get("skipRowIfMissing")
                     valueToPlace = (
                         self.rowSelected[col].get("Value")
                         or self.ws[col + str(self.StartRow + row)].value
-                    ) # try to set default value, if not checks cell
-                    if valueToPlace:
-                        variabeler = self.rowSelected[col].get("targetColumn")
-                        if isinstance(variabeler,list):
-                            for variabel in variabeler:
-                                self.new_ws[
-                                    variabel
-                                    + str(self.NewWorkBookCurrentRow)
-                                ] = valueToPlace  # writes valueToPlace to New WB
-                        else:
-                            self.new_ws[
-                                    variabeler
-                                    + str(self.NewWorkBookCurrentRow)
-                                ] = valueToPlace  # writes valueToPlace to New WB
-                            
-                    if (self.skipRowIfMissingFlagg and not valueToPlace):
-                        self.errors.append(f"Skipping row {i} {col}{self.StartRow + row} is missing given")
-                        self.skippedRows -= 1 # decreasing the row counter so next row will not go passed the current
-                        break
-                    elif (
-                        self.rowSelected[col].get("mandatory") and not valueToPlace
-                    ):  # listing every field that is missing mandatory data
-                        self.errors.append(
-                            f"{col}{self.StartRow + row} is mandatory but no value given"
-                        )
-                        self.new_ws[
-                            self.rowSelected[col].get("targetColumn")
-                            + str(self.NewWorkBookCurrentRow)
-                        ].fill = self.errorColor # Colors every field that is missing mandatory data
+                    )
+                    self.skipRowIfMissingFlagg = True if self.rowSelected[col].get("skipRowIfMissing") and not valueToPlace else False
 
-    def SaveFiles(self):
-        if self.errors and len(self.errors) < 5:  # showing missing data as a messagebox warning
-            messagebox.showwarning(
-                title="Missing data in workbook",
-                message=f"""{self.filename}\n{"\n".join(self.errors)}""",
-            )
-        elif self.errors:
-            messagebox.showwarning(
-                title="Missing data in workbook",
-                message=f"""{self.filename}\nmissing data in {len(self.errors)} mandatory fields""",
-            )
-        saveFileName = filedialog.asksaveasfilename(
+                    if valueToPlace:
+                        target_columns = self.rowSelected[col].get("targetColumn")
+                        
+                        # Hvis targetColumn er en liste, iterer over den
+                        if isinstance(target_columns, list):
+                            for target_col in target_columns:
+                                self.new_ws[target_col + str(self.NewWorkBookCurrentRow)] = valueToPlace
+                        else:
+
+                            self.new_ws[target_columns + str(self.NewWorkBookCurrentRow)] = valueToPlace
+                    if self.skipRowIfMissingFlagg:
+                        self.deleteIfRowFails.append(self.NewWorkBookCurrentRow)
+
+        self.deleteIfRowFails.sort()
+        while self.deleteIfRowFails:
+            deleteRow = self.deleteIfRowFails.pop()
+            print(f"deleting row {deleteRow}")
+            self.new_ws.delete_rows(deleteRow)
+
+
+    def saveFile(self):
+        """Lagre filen med dato og tid."""
+        now = datetime.datetime.now()
+        date_str = now.strftime("%d%m%H%M")  # Format: ddmmyyhhmm
+
+        # Mappe for å lagre filen som er klar for innlesning
+        save_path = filedialog.asksaveasfilename(
             title="Save file as",
-            filetypes=[("Excel filer", "*.xlsx")],
-            defaultextension=".xlsx",
+            filetypes=[("Excel filer", "*.xlsm")],
+            defaultextension=".xlsm",
         )
-        if saveFileName:  # if no save file is set ignores save and open of Excel
-            try:
-                self.new_wb.save(saveFileName)
-                savePath = Path(saveFileName)
-                os.system(f'start EXCEL.EXE "{savePath}"')
-            except PermissionError:
-                messagebox.showwarning(title="Fil locked",message="Filen er alt åpen")
-        self.new_wb.close()  # closes the new wb
-        self.wb.close()  # closes the existing wb
+
+        # Filnavn som settes ved lagring av filen, lagres som .xlsm pga. makroer i innlesningsfil 
+        #save_path = os.path.join(target_folder, f"Innlesing_FK{date_str}.xlsm")
+
+        try:
+            self.new_wb.save(save_path)  # Lagre filen på ønsket sted
+            messagebox.showinfo("Fil Lagret", f"Filen er lagret som: {save_path}")
+            os.system(f'start EXCEL.EXE "{save_path}"')  # Åpne filen i Excel ved lagring for å se
+        except PermissionError:
+            messagebox.showwarning(title="Fil er låst", message="Filen er allerede åpen. Lukk den og prøv igjen.")
 
     def __init__(self):
         self.errorColor = openpyxl.styles.fills.PatternFill(
@@ -127,13 +135,14 @@ class transformExcel:
         self.errors = []
         self.getInputData()
         self.variableSetup()
+        self.deleteIfRowFails = ""
         self.skippedRows = 0
         if self.ws:
             self.setupNewFile()
             self.readAndWriteExcel()
-            self.SaveFiles()
+            self.saveFile()  # Lagre filen
         else:
-            messagebox.showwarning("Worksheet not found",message="Could not find the worksheet")
+            messagebox.showwarning("Arbeidsark ikke funnet", message="Kunne ikke finne arbeidsarket")
 
 if __name__ == "__main__":
     transformExcel()
